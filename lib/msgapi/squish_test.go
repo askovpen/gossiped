64ffc9ae6555ac17d5b85fe5d84e582eae0a0fd8
Alexander Skovpen <a.n.skovpen@gmail.com>
2018-08-22 20:54:53 +0300

    fix squish hash

--- lib/msgapi/squish_test.go
+++ lib/msgapi/squish_test.go
@@ -13,5 +13,8 @@ func TestSquishBufHash32(t *testing.T) {
 		Convey("calculates correct Sqush bufhash-32 of the string 'Alexander N. Skovpen'", func() {
 			So(0x00efd7be, ShouldEqual, bufHash32("Alexander N. Skovpen"))
 		})
+		Convey("calculates correct Sqush bufhash-32 of the string 'Эдуардыч'[CP866]", func() {
+			So(0x02debb97, ShouldEqual, bufHash32(string([]byte{'\x9D','\xA4', '\xE3', '\xA0', '\xE0', '\xA4', '\xEB', '\xE7'})))
+		})
 	})
 }
