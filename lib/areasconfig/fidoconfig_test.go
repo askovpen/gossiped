54c70a285e6ddb4b1cb03d83d1784fe799966e1b
Alexander Skovpen <askovpen@users.noreply.github.com>
2018-09-01 00:03:38 +0300

    add testdata (#76)
    * add testdata
    
    * add coverage

--- lib/areasconfig/fidoconfig_test.go
+++ lib/areasconfig/fidoconfig_test.go
@@ -1,17 +1,23 @@
 package areasconfig
 
 import (
+	"github.com/askovpen/goated/lib/msgapi"
 	. "github.com/franela/goblin"
 	"os"
 	"testing"
 )
 
 func TestFidoConfig(t *testing.T) {
+	msgapi.Areas = msgapi.Areas[:0]
 	g := Goblin(t)
 	g.Describe("Check FidoConfig", func() {
 		g.It("check replaceEnv()", func() {
 			os.Setenv("TESTENV", "PASSED")
 			g.Assert(replaceEnv("[TESTENV]")).Equal("PASSED")
+		})
+		g.It("check fidoConfigRead()", func() {
+			fidoConfigRead("../../testdata/hpt.areas")
+			g.Assert(len(msgapi.Areas)).Equal(18)
 		})
 	})
 }
---
+++ lib/areasconfig/squish_test.go
@@ -0,0 +1,18 @@
+package areasconfig
+
+import (
+	"github.com/askovpen/goated/lib/msgapi"
+	. "github.com/franela/goblin"
+	"testing"
+)
+
+func TestSquishConfig(t *testing.T) {
+	msgapi.Areas = msgapi.Areas[:0]
+	g := Goblin(t)
+	g.Describe("Check SquishConfig", func() {
+		g.It("check squishConfigRead()", func() {
+			squishConfigRead("../../testdata/squish.cfg")
+			g.Assert(len(msgapi.Areas)).Equal(56)
+		})
+	})
+}
