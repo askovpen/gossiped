5af7ecd0062ee23574341b9540d86023fa006c58
Alexander Skovpen <askovpen@users.noreply.github.com>
2018-09-01 20:47:02 +0300

    squish create base (#79)
    * squish create base
    
    * remove unneeded

--- lib/msgapi/squish_test.go
+++ lib/msgapi/squish_test.go
@@ -1,8 +1,11 @@
 package msgapi
 
 import (
+	"github.com/askovpen/goated/lib/types"
 	. "github.com/franela/goblin"
 	"testing"
+	"os"
+	"time"
 )
 
 func TestSquishBufHash32(t *testing.T) {
@@ -21,6 +24,51 @@ func TestSquishBufHash32(t *testing.T) {
 			g.Assert(bufHash32(string([]byte{'\x9E', '\xE0', '\xA8', '\xA9', '\x20', '\x83', '\xE0', '\xA8', '\xA3', '\xAE', '\xE0', '\xEC', '\xA5', '\xA2'}))).Equal(uint32(0x7c100ff2))
 		})
 	})
+
+}
+func TestSquish(t *testing.T) {
+	Area := &Squish{
+		AreaPath: "../../testdata/sqtest",
+		AreaName: "test",
+		AreaType: EchoAreaTypeEcho,
+	}
+	g := Goblin(t)
+	g.Describe("Check Squish read/write", func() {
+		m := &Message{
+			AreaID:      0,
+			From:        "SysOp",
+			To:          "SysOp",
+			Subject:     "Test",
+			FromAddr:    types.AddrFromNum(2, 5020, 9696, 1),
+			ToAddr:      types.AddrFromNum(2, 5020, 9696, 2),
+			DateWritten: time.Now(),
+			DateArrived: time.Now(),
+			Body:        "Test\nBody",
+			Kludges:     make(map[string]string),
+		}
+		m.MakeBody()
+		g.It("create msg", func() {
+			g.Assert(Area.SaveMsg(m)).Equal(nil)
+		})
+		g.It("add msg", func() {
+			g.Assert(Area.SaveMsg(m)).Equal(nil)
+		})
+		g.It("check num msgs", func() {
+			g.Assert(Area.GetCount()).Equal(uint32(2))
+		})
+		g.It("read msg", func() {
+			nm, err := Area.GetMsg(1)
+			g.Assert(err).Equal(nil)
+			g.Assert(nm.FromAddr).Equal(types.AddrFromNum(2, 5020, 9696, 1))
+		})
+		g.It("get/set last", func() {
+			Area.SetLast(1)
+			g.Assert(Area.GetLast()).Equal(uint32(1))
+		})
+	})
+	os.Remove("../../testdata/sqtest.sqd")
+	os.Remove("../../testdata/sqtest.sqi")
+	os.Remove("../../testdata/sqtest.sql")
 }
 func BenchmarkSquishBufHash32(b *testing.B) {
 	b.SetBytes(20)
