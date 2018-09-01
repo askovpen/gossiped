18ef97ac916b5c590881cf9e892227d9c6ccc96e
Alexander Skovpen <a.n.skovpen@gmail.com>
2018-09-01 22:11:56 +0300

    coverage fidoaddr

--- lib/types/fidoaddr_test.go
+++ lib/types/fidoaddr_test.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	. "github.com/franela/goblin"
+	"gopkg.in/yaml.v2"
 	"testing"
 )
 
@@ -41,6 +42,15 @@ func TestFidoAddr(t *testing.T) {
 			g.Assert(err.Error()).Equal("point")
 			f,err:=(&FidoAddr{2, 5020, 9696, 0}).FQDN()
 			g.Assert(f).Equal("f9696.n5020.z2.binkp.net")
+		})
+		g.It("check yaml", func() {
+			f:=&FidoAddr{2, 5020, 9696, 0}
+			d, err := yaml.Marshal(f)
+			g.Assert(err).Equal(nil)
+			g.Assert(d).Equal([]byte{0x32, 0x3a, 0x35, 0x30, 0x32, 0x30, 0x2f, 0x39, 0x36, 0x39, 0x36, 0xa})
+			err = yaml.Unmarshal(d, f)
+			g.Assert(err).Equal(nil)
+			g.Assert(f).Equal(&FidoAddr{2, 5020, 9696, 0})
 		})
 	})
 }
