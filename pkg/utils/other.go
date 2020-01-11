216ca9a74f1e6e879dd9f4aa88c9e84074732011
Alexander Skovpen <a.n.skovpen@gmail.com>
2020-01-11 14:32:20 +0300

    fix errors

---
+++ pkg/utils/other.go
@@ -0,0 +1,13 @@
+package utils
+
+import (
+	"os"
+)
+
+func FileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
