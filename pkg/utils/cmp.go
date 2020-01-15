35dbffda47b9e31e77c831faa85f787ef3709788
Alexander Skovpen <a.n.skovpen@gmail.com>
2020-01-15 17:29:15 +0300

    fix highlight

--- pkg/utils/cmp.go
+++ pkg/utils/cmp.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 )
 
+// NamesEqual compare names
 func NamesEqual(f string, s string) bool {
 	if strings.Replace(strings.Trim(f, " "), ".", "", -1) == strings.Replace(strings.Trim(s, " "), ".", "", -1) {
 		return true
