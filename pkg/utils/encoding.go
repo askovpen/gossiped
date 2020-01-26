115282bf40d4fad126f5f49788301dc29c727db1
Alexander Skovpen <askovpen@users.noreply.github.com>
2020-01-26 19:45:49 +0300

    lint (#157)
    * lint fidocondig
    
    * improve charmap

--- pkg/utils/encoding.go
+++ pkg/utils/encoding.go
@@ -1,43 +1,60 @@
 package utils
 
 import (
+	"io/ioutil"
+	"strings"
+
 	"golang.org/x/text/encoding"
 	"golang.org/x/text/encoding/charmap"
 	"golang.org/x/text/transform"
-	"io/ioutil"
-	"strings"
+)
+
+var (
+	cDecoder = map[string]*encoding.Decoder{
+		"CP866":   charmap.CodePage866.NewDecoder(),
+		"+7_FIDO": charmap.CodePage866.NewDecoder(),
+		"+7":      charmap.CodePage866.NewDecoder(),
+		"IBM866":  charmap.CodePage866.NewDecoder(),
+		"CP850":   charmap.CodePage850.NewDecoder(),
+		"CP852":   charmap.CodePage852.NewDecoder(),
+		"CP848":   charmap.CodePage866.NewDecoder(),
+		"CP1250":  charmap.Windows1250.NewDecoder(),
+		"CP1251":  charmap.Windows1251.NewDecoder(),
+		"CP1252":  charmap.Windows1252.NewDecoder(),
+		"CP10000": charmap.Macintosh.NewDecoder(),
+		"CP437":   charmap.CodePage437.NewDecoder(),
+		"IBMPC":   charmap.CodePage437.NewDecoder(),
+		"LATIN-2": charmap.ISO8859_2.NewDecoder(),
+		"LATIN-5": charmap.ISO8859_5.NewDecoder(),
+		"LATIN-9": charmap.ISO8859_9.NewDecoder(),
+	}
+	cEncoder = map[string]*encoding.Encoder{
+		"CP866":   charmap.CodePage866.NewEncoder(),
+		"+7_FIDO": charmap.CodePage866.NewEncoder(),
+		"+7":      charmap.CodePage866.NewEncoder(),
+		"IBM866":  charmap.CodePage866.NewEncoder(),
+		"CP850":   charmap.CodePage850.NewEncoder(),
+		"CP852":   charmap.CodePage852.NewEncoder(),
+		"CP848":   charmap.CodePage866.NewEncoder(),
+		"CP1250":  charmap.Windows1250.NewEncoder(),
+		"CP1251":  charmap.Windows1251.NewEncoder(),
+		"CP1252":  charmap.Windows1252.NewEncoder(),
+		"CP10000": charmap.Macintosh.NewEncoder(),
+		"CP437":   charmap.CodePage437.NewEncoder(),
+		"IBMPC":   charmap.CodePage437.NewEncoder(),
+		"LATIN-2": charmap.ISO8859_2.NewEncoder(),
+		"LATIN-5": charmap.ISO8859_5.NewEncoder(),
+		"LATIN-9": charmap.ISO8859_9.NewEncoder(),
+	}
 )
 
 // DecodeCharmap decode string from charmap
 func DecodeCharmap(s string, c string) string {
 	sr := strings.NewReader(s)
 	var tr *transform.Reader
-	switch strings.ToUpper(c) {
-	case "CP866", "+7_FIDO", "+7", "IBM866":
-		tr = transform.NewReader(sr, charmap.CodePage866.NewDecoder())
-	case "CP850":
-		tr = transform.NewReader(sr, charmap.CodePage850.NewDecoder())
-	case "CP852":
-		tr = transform.NewReader(sr, charmap.CodePage852.NewDecoder())
-	case "CP848":
-		// CP848 IBM codepage 848 (Cyrillic Ukrainian) -> to be added as XUserDefined
-		tr = transform.NewReader(sr, charmap.CodePage866.NewDecoder())
-	case "CP1250":
-		tr = transform.NewReader(sr, charmap.Windows1250.NewDecoder())
-	case "CP1251":
-		tr = transform.NewReader(sr, charmap.Windows1251.NewDecoder())
-	case "CP1252":
-		tr = transform.NewReader(sr, charmap.Windows1252.NewDecoder())
-	case "CP10000":
-		tr = transform.NewReader(sr, charmap.Macintosh.NewDecoder())
-	case "CP437", "IBMPC":
-		tr = transform.NewReader(sr, charmap.CodePage437.NewDecoder())
-	case "LATIN-2":
-		tr = transform.NewReader(sr, charmap.ISO8859_2.NewDecoder())
-	case "LATIN-5":
-		tr = transform.NewReader(sr, charmap.ISO8859_5.NewDecoder())
-	case "LATIN-9":
-		tr = transform.NewReader(sr, charmap.ISO8859_9.NewDecoder())
+	switch chrs := strings.ToUpper(c); chrs {
+	case "CP866", "+7_FIDO", "+7", "IBM866", "CP850", "CP852", "CP848", "CP1250", "CP1251", "CP1252", "CP10000", "CP437", "IBMPC", "LATIN-2", "LATIN-5", "LATIN-9":
+		tr = transform.NewReader(sr, cDecoder[chrs])
 	case "UTF-8":
 		return s
 	default:
@@ -54,30 +71,8 @@ func DecodeCharmap(s string, c string) string {
 func EncodeCharmap(s string, c string) string {
 	var enc *encoding.Encoder
 	switch c {
-	case "CP866", "+7_FIDO", "+7":
-		enc = charmap.CodePage866.NewEncoder()
-	case "CP850":
-		enc = charmap.CodePage850.NewEncoder()
-	case "CP852":
-		enc = charmap.CodePage852.NewEncoder()
-	case "CP848":
-		enc = charmap.CodePage866.NewEncoder()
-	case "CP1250":
-		enc = charmap.Windows1250.NewEncoder()
-	case "CP1251":
-		enc = charmap.Windows1251.NewEncoder()
-	case "CP1252":
-		enc = charmap.Windows1252.NewEncoder()
-	case "CP10000":
-		enc = charmap.Macintosh.NewEncoder()
-	case "CP437", "IBMPC":
-		enc = charmap.CodePage437.NewEncoder()
-	case "LATIN-2":
-		enc = charmap.ISO8859_2.NewEncoder()
-	case "LATIN-5":
-		enc = charmap.ISO8859_5.NewEncoder()
-	case "LATIN-9":
-		enc = charmap.ISO8859_9.NewEncoder()
+	case "CP866", "+7_FIDO", "+7", "IBM866", "CP850", "CP852", "CP848", "CP1250", "CP1251", "CP1252", "CP10000", "CP437", "IBMPC", "LATIN-2", "LATIN-5", "LATIN-9":
+		enc = cEncoder[c]
 	case "UTF-8":
 		return s
 	default:
